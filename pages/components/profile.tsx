2ff817d547a5b7598daab9038481fb1b9735c6ff
erikmint <erik.t3600@hotmail.com>
2022-09-26 16:21:32 -0300

    Profile component types modified.

--- pages/components/profile.tsx
+++ pages/components/profile.tsx
@@ -1,10 +1,19 @@
 export default function Profile(props): JSX.Element {
+
+    function getWhiteFont(value: string): string {
+        if (value === "normal" || value === "ground" ||
+            value === 'flying' || value === 'electric' ||
+            value === 'ice' || value === 'fairy') return ''
+        
+        return 'text-white'
+    }
+
     return (
         //If pokemon state has no data, all the component is hidden
         <div className='profile mx-3'>
             <img alt="" src={props.img} />
             <ul className="d-flex ps-0 list-group list-group-horizontal rounded">
-                {props.types.map(value => <li className="profile-type text-center list-group-item flex-fill" style={{ backgroundColor: 'var(--' + value + ')' }} key={Math.random()}>{value.toUpperCase()}</li>)}
+                {props.types.map(value => <li className={`profile-type fs-5 fw-bold text-center list-group-item flex-fill ${getWhiteFont(value)}`} style={{ backgroundColor: 'var(--' + value + ')' }} key={Math.random()}>{value.toUpperCase()}</li>)}
             </ul>
         </div>
     )
