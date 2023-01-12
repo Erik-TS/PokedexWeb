f3dd96d47b0bab1edab5618c3942291287e2c142
erikmint <erik.t3600@hotmail.com>
2023-01-12 07:17:25 -0300

    Remove the InfoBlock component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,8 @@
 import { SetStateAction, useEffect, useState } from 'react'
 import Topbar from './components/topbar'
-import InfoBlock from './components/InfoBlock'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Router from 'next/router'
 import ProfileArea from './components/profileArea'
-
-function H(): JSX.Element {
-    const [poke, setPoke] = useState('')
-    const cliente: QueryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                refetchOnWindowFocus: false
-            }
-        }
-    })
-
-    function handlePoke(value: SetStateAction<string>): void {
-        setPoke(value)
-    }
-
-    function searchPoke(): void {
-        const searchInp: string = (document.querySelector('.idNameInput') as HTMLInputElement).value.toLocaleLowerCase()
-        handlePoke(searchInp)
-    }
-
-    //Press Enter trigger fetchPokeApi()
-    useEffect(() => {
-        const { pathname } = Router
-        if (pathname == '/') {
-            Router.push('/pikachu')
-        }
-        let listener = (e: { key: string }) => e.key === 'Enter' && searchPoke()
-        document.addEventListener('keydown', listener)
-
-        //The 'useEffect' needs to be cleaned so as not to trigger effects several times
-        return () => document.removeEventListener('keydown', listener)
-    })
-
-    return (
-        <div>
-            <Topbar search={searchPoke} />
-            <QueryClientProvider client={cliente}>
-                <InfoBlock key={poke} search={poke} />
-            </QueryClientProvider>
-        </div>
-    )
-}
 
 export default function Home() {
     const [loading, setLoading] = useState(false)
