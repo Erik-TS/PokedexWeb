17bcdaf69270196593fb400b7bc027b9b7fa1a56
erikmint <erik.t3600@hotmail.com>
2023-03-21 17:10:59 -0300

    Change the pokemon images to those provided by the API.

--- pages/api/info.ts
+++ pages/api/info.ts
@@ -3,53 +3,22 @@ import { MainClient } from "pokenode-ts";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    function getImgUrls(response, hasVarieties: boolean, varieties: number) {
-        const urlList: Array<string> = []
-        let imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
-        let fileSuffix = '.png'
-        let fileId: string
-
-        if (response.id >= 100) fileId = `${response.id}`
-        else if (response.id >= 10) fileId = `0${response.id}`
-        else fileId = `00${response.id}`
-
-        if (hasVarieties) {
-            for (let i = 0; i < varieties; i++) {
-                if (i > 0) fileSuffix = `_f${i + 1}.png`
-                imgUrl += `${fileId}${fileSuffix}`
-                urlList.push(imgUrl)
-                imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
-            }
-        }
-        else {
-            imgUrl += `${fileId}${fileSuffix}`
-            urlList.push(imgUrl)
-        }
-
-        return urlList
-    }
-
-    const r: { name: string } = JSON.parse(req.body)
-    let search = r.name.toLowerCase()
-    const arr = []
+    const requestBody: { name: string } = JSON.parse(req.body)
+    const userInput = requestBody.name.toLowerCase()
+    const foundPokemon = []
     const api = new MainClient()
 
     await api.pokemon
-        .getPokemonSpeciesByName(search)
+        .getPokemonSpeciesByName(userInput)
         .then(async data => {
-            const varieties = data.varieties.length
-            const hasVarieties = varieties > 1
-            let imgs = getImgUrls(data, hasVarieties, varieties)
-
-            for (let x of data.varieties) {
+            for (let PokemonSpeciesVariety of data.varieties) {
                 await api.pokemon
-                    .getPokemonByName(x.pokemon.name)
+                    .getPokemonByName(PokemonSpeciesVariety.pokemon.name)
                     .then(poke => {
-                        const imgUrl = imgs.shift()
-                        arr.push({ ...poke, imgUrl: imgUrl })
+                        foundPokemon.push({ ...poke, imgUrl: poke.sprites.other["official-artwork"].front_default })
                     })
             }
-            res.status(200).send(JSON.stringify(arr))
+            res.status(200).send(JSON.stringify(foundPokemon))
         })
         .catch(err => res.status(400).send(err))
 }
